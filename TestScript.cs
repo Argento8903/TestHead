18d66cd53159a07510916a29d9382fb172ac2128
Argento- <nandakaiikanjijidego@gmail.com>
2025-05-24 23:26:34 +0900

    特定のブランチをピンポイントでGitURLで取得できるか確認するためのテストデータをプッシュ

--- TestScript.cs
+++ TestScript.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+
+    /// <summary>
+    /// このスクリプトはTestブランチにのみプッシュされています。
+    /// </summary>
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
